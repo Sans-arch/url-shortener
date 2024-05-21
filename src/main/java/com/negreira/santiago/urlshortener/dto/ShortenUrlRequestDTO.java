eff19f2b466f42cb0a6d422651ef4d8b6f359cc6
Santiago Negreira <sansfleitas@gmail.com>
2024-05-21 17:53:35 -0300

    Create ShortenUrlDTOs

---
+++ src/main/java/com/negreira/santiago/urlshortener/dto/ShortenUrlRequestDTO.java
@@ -0,0 +1,8 @@
+package com.negreira.santiago.urlshortener.dto;
+
+import lombok.Data;
+
+@Data
+public class ShortenUrlRequestDTO {
+    private String originalUrl;
+}
---
+++ src/main/java/com/negreira/santiago/urlshortener/dto/ShortenUrlResponseDTO.java
@@ -0,0 +1,10 @@
+package com.negreira.santiago.urlshortener.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ShortenUrlResponseDTO {
+    private String shortUrl;
+}
